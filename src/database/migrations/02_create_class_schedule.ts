fea1e6a0c58647a030134d1b18cecafaf44334a2
lchirata <larissa.c.hirata@gmail.com>
2020-08-05 15:46:54 -0300

    add table connections e schedule

---
+++ src/database/migrations/02_create_class_schedule.ts
@@ -0,0 +1,25 @@
+import Knex from 'knex';
+
+//fazer alterações
+export async function up(knex: Knex){
+    return knex.schema.createTable('classes_schedule', table => {
+        table.increments('id').primary();
+
+        table.integer('week_day').notNullable();
+        table.integer('from').notNullable();
+        table.integer('to').notNullable();
+
+
+        table.integer('class_id')
+            .notNullable()
+            .references('id')
+            .inTable('classes')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE');
+    });
+}
+
+//desfazer alterações
+export async function down(knex: Knex){
+    return knex.schema.dropTable('classes_schedule');
+}
